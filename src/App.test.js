ac9d5902c54a47823ca9262e6ad4e8474dbc56e8
Chandra Damayanthi <cdamayanthi819@gmail.com>
2023-11-01 13:35:52 +0530

    Add a heading to display the welcome message

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render heading with welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Welcome')
   expect(linkElement).toBeInTheDocument();
 });
 
